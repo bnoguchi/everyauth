5cc00fd44d603e89d65bd3782e198ffa8b3ebec6
bnoguchi <brian.noguchi@gmail.com>
2011-03-24 17:06:56 -0700

    Fixed callback copying for everymodule submodules.

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-var everyauth = module.exports = function (app) {
-  this.app = app;
-  this.refresh();
+var connect = require('connect');
+
+var everyauth = module.exports = function () {
+  var app = connect(
+      function (req, res, next) {
+        req.isAuth = everyauth.isAuth;
+        next();
+      }
+    , connect.router(function (app) {
+        var modules = everyauth.modules
+          , _module;
+        for (var _name in modules) {
+          _module = modules[_name];
+          _module.routeApp(app);
+        }
+      })
+  );
+  return app;
 };
 
-everyauth.refresh = function () {
-  var modules = everyauth.modules;
-  for (var _module in modules) {
-    modules[_module].routeApp(app);
-  }
-};
-
-['facebook'].forEach( function (name) {
-  everyauth[name] = 
+everyauth.modules = {};
+['facebook'].forEach( function(name) {
+  everyauth[name] =
   everyauth.modules[name] = require('./lib/' + name);
 });
+
+everyauth.isAuth = function () {
+  // TODO
+  return false;
+};
