4ed142d8a142d951f652f0d1c112ce1f9ad8e69c
Brian Noguchi <brian.noguchi@gmail.com>
2012-02-25 16:42:53 -0800

    Convert facebook test to expect.js and mocha

--- test/facebook.test.js
+++ test/facebook.test.js
@@ -1,34 +1,43 @@
 var tobi = require('tobi')
-  , should = require('should')
+  , expect = require('expect.js')
   , browser = tobi.createBrowser(3000, 'local.host')
   , creds = require('./creds.js');
 
+require('./util/expect.js');
+
 // Test a successful login
-browser.userAgent = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.100 Safari/534.30';
-browser.get('/auth/facebook', function (res, $) {
-  var form = $('#login_form');
-  form
-    .fill({ 
-        email: creds.facebook.login
-      , pass: creds.facebook.password
+describe('a successful login', function () {
+  it ('should succeed', function (done) {
+    this.timeout(10000);
+    browser.userAgent = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.100 Safari/534.30';
+    browser.get('/auth/facebook', function (res, $) {
+      var form = $('#login_form');
+      form
+        .fill({ 
+            email: creds.facebook.login
+          , pass: creds.facebook.password
 
-        // Need to do this; otherwise, parameters are corrupted, and Facebook responds with an error
-        // Ideally, $.fn.serializeArray() would do this, but alas...
-      , next: form.find('input[name="next"]').attr('value').replace(/&amp;/g, '&') })
-    .submit(function (res, $) {
-      var form = $('#uiserver_form');
-      if (form.length) {
-        // If we are at the "Request for Permission" page on Facebook
-        form.submit( function (res, $) {
-          $('h2').should.have.text('Authenticated');
-          $('h2').should.not.have.text('Not Authenticated');
+            // Need to do this; otherwise, parameters are corrupted, and Facebook responds with an error
+            // Ideally, $.fn.serializeArray() would do this, but alas...
+          , next: form.find('input[name="next"]').attr('value').replace(/&amp;/g, '&') })
+        .submit(function (res, $) {
+          var form = $('#uiserver_form');
+          if (form.length) {
+            // If we are at the "Request for Permission" page on Facebook
+            return form.submit( function (res, $) {
+              expect($('h2')).to.have.text('Authenticated');
+              expect($('h2')).not.to.have.text('Not Authenticated');
+              done();
+            });
+
+          }
+
+          // Else we are back at local.host
+          expect($('h2')).to.have.text('Authenticated');
+          expect($('h2')).to.not.have.text('Not Authenticated');
+          done();
+
         });
-
-      } else {
-        // Else we are back at local.host
-        $('h2').should.have.text('Authenticated');
-        $('h2').should.not.have.text('Not Authenticated');
-      }
-
     });
+  });
 });
