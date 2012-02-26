c3fbaa52787d6a3fc964d688ecd7f421f6178580
Brian Noguchi <brian.noguchi@gmail.com>
2012-02-25 16:43:17 -0800

    Convert password tests to expect.js and mocha

--- test/password.test.js
+++ test/password.test.js
@@ -1,78 +1,128 @@
 var tobi = require('tobi')
-  , should = require('should')
+  , expect = require('expect.js')
   , browser = tobi.createBrowser(3000, 'local.host');
 
-// Test a successful registration
-browser.get('/register', function (res, $) {
-  $('form')
-    .fill({ email: 'newuser@example.com', password: 'pass' })
-    .submit( function (res, $) {
-      res.should.have.status(200);
-      $('h2').should.have.text('Authenticated');
-      $('h2').should.not.have.text('Not Authenticated');
-    });
-});
+require('./util/expect.js');
 
-// Test failed registrations
-browser.get('/register', function (res, $) {
-  $('form')
-    .fill({ email: '', password: '' })
-    .submit( function (res, $) {
-      res.should.have.status(200);
-      $('#errors li:first').should.have.text('Missing email');
-      $('#errors li:eq(1)').should.have.text('Missing password');
-    });
-  $('form')
-    .fill({ email: 'newuser', password: 'pass' })
-    .submit( function (res, $) {
-      res.should.have.status(200);
-      $('#errors').should.have.text('Please correct your email.');
-    });
-  $('form')
-    .fill({ email: 'newuser', password: '' })
-    .submit( function (res, $) {
-      res.should.have.status(200);
-      $('#errors li:first').should.have.text('Please correct your email.');
-      $('#errors li:eq(1)').should.have.text('Missing password');
-    });
-  $('form')
-    .fill({ email: 'abc@example.com', password: '' })
-    .submit( function (res, $) {
-      res.should.have.status(200);
-      $('#errors').should.have.text('Missing password');
+describe('password', function () {
+  describe('registration', function () {
+    it('should succeed if provided with a username and password', function (done) {
+      browser.get('/register', function (res, $) {
+        $('form')
+          .fill({ email: 'newuser@example.com', password: 'pass' })
+          .submit( function (res, $) {
+            res.should.have.status(200);
+            $('h2').should.have.text('Authenticated');
+            $('h2').should.not.have.text('Not Authenticated');
+            done();
+          });
+      });
     });
 
-  // TODO Add case of person trying to take an existing login
-});
+    describe('failing', function () {
+      it('should fail if no email, no password', function (done) {
+        browser.get('/register', function (res, $) {
+          $('form')
+            .fill({ email: '', password: '' })
+            .submit( function (res, $) {
+              res.should.have.status(200);
+              $('#errors li:first').should.have.text('Missing email');
+              $('#errors li:eq(1)').should.have.text('Missing password');
+              done();
+            });
+        });
 
-// Test a successful login
-browser.get('/login', function (res, $) {
-  $('form')
-    .fill({ email: 'brian@example.com', password: 'password' })
-    .submit( function (res, $) {
-      res.should.have.status(200);
-      $('h2').should.have.text('Authenticated');
-      $('h2').should.not.have.text('Not Authenticated');
-    });
-});
+        // TODO Add case of person trying to take an existing login
+      });
 
-// Test failed logins
-browser.get('/login', function (res, $) {
-  $('form')
-    .fill({ email: 'brian@example.com', password: 'wrongpassword' })
-    .submit( function (res, $) {
-      res.should.have.status(200);
-      $('#errors').should.have.text('Login failed');
+      it('should fail with an invalid email, non-empty password', function (done) {
+        browser.get('/register', function (res, $) {
+          $('form')
+            .fill({ email: 'newuser', password: 'pass' })
+            .submit( function (res, $) {
+              res.should.have.status(200);
+              $('#errors').should.have.text('Please correct your email.');
+              done();
+            });
+        });
+      });
+
+      it('should fail with an invalid email, no password', function (done) {
+        browser.get('/register', function (res, $) {
+          $('form')
+            .fill({ email: 'newuser', password: '' })
+            .submit( function (res, $) {
+              res.should.have.status(200);
+              $('#errors li:first').should.have.text('Please correct your email.');
+              $('#errors li:eq(1)').should.have.text('Missing password');
+              done();
+            });
+        });
+      });
+
+      it('should fail with a valid email, no password', function (done) {
+        browser.get('/register', function (res, $) {
+          $('form')
+            .fill({ email: 'abc@example.com', password: '' })
+            .submit( function (res, $) {
+              res.should.have.status(200);
+              $('#errors').should.have.text('Missing password');
+              done();
+            });
+        });
+      });
     });
-  $('form')
-    .fill({ email: 'brian@example.com', password: '' })
-    .submit( function (res, $) {
-      $('#errors').should.have.text('Missing password');
+  });
+
+  describe('login', function () {
+    it('should succeed with the right email + password', function (done) {
+      browser.get('/login', function (res, $) {
+        $('form')
+          .fill({ email: 'brian@example.com', password: 'password' })
+          .submit( function (res, $) {
+            res.should.have.status(200);
+            $('h2').should.have.text('Authenticated');
+            $('h2').should.not.have.text('Not Authenticated');
+            done();
+          });
+      });
     });
-  $('form')
-    .fill({ email: '', password: '' })
-    .submit( function (res, $) {
-      $('#errors li:first').should.have.text('Missing login');
-      $('#errors li:eq(1)').should.have.text('Missing password');
+
+    describe('failing', function () {
+      it('should fail with the wrong password', function (done) {
+        browser.get('/login', function (res, $) {
+          $('form')
+            .fill({ email: 'brian@example.com', password: 'wrongpassword' })
+            .submit( function (res, $) {
+              res.should.have.status(200);
+              $('#errors').should.have.text('Login failed');
+              done();
+            });
+        });
+      });
+
+      it('should fail with an empty password', function (done) {
+        browser.get('/login', function (res, $) {
+          $('form')
+            .fill({ email: 'brian@example.com', password: '' })
+            .submit( function (res, $) {
+              $('#errors').should.have.text('Missing password');
+              done();
+            });
+        });
+      });
+
+      it('should fail with no email, no password', function (done) {
+        browser.get('/login', function (res, $) {
+          $('form')
+            .fill({ email: '', password: '' })
+            .submit( function (res, $) {
+              $('#errors li:first').should.have.text('Missing login');
+              $('#errors li:eq(1)').should.have.text('Missing password');
+              done();
+            });
+          });
+      });
     });
+  });
 });
